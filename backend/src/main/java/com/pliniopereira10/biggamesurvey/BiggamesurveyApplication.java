ea06fad39b8674f85f44811a34b4eb08bfde9e5b
pliniopereira10 <plinio.pereira10@gmail.com>
2022-07-29 20:13:36 -0300

    Project created

---
+++ backend/src/main/java/com/pliniopereira10/biggamesurvey/BiggamesurveyApplication.java
@@ -0,0 +1,13 @@
+package com.pliniopereira10.biggamesurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiggamesurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiggamesurveyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pliniopereira10/biggamesurvey/BiggamesurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pliniopereira10.biggamesurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiggamesurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
