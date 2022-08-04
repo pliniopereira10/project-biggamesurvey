99be261024f8e1a46eabe21961fb0c62137b619e
pliniopereira10 <plinio.pereira10@gmail.com>
2022-08-04 10:00:18 -0300

    DTO, Service

--- backend/src/main/java/com/pliniopereira10/biggamesurvey/repositories/GenreRepository.java
+++ backend/src/main/java/com/pliniopereira10/biggamesurvey/repositories/GenreRepository.java
@@ -1,9 +1,11 @@
 package com.pliniopereira10.biggamesurvey.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pliniopereira10.biggamesurvey.entities.Genre;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
 }
